e5aeca3b48fdcac9eb37e57658ac17a67e04211f
rasheed <rasheedoduola007@gamil.com>
2025-03-23 15:37:17 +0100

    feat: implement intersection observer for fade-in effect on project sections

--- app.js
+++ app.js
@@ -5,3 +5,14 @@ const downDropMenu = document.querySelector('.dropdown_menu')
 toggleBtn.onclick = function () {
     downDropMenu.classList.toggle('open')
 }
+const hiddenTexts = document.querySelectorAll(".app, .app0, .app1, .app2, .app3, .app4");
+
+const observer = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add("show");
+        }
+    });
+});
+
+hiddenTexts.forEach(hiddenText => observer.observe(hiddenText));
