c1ef9370528c6c018d2e8a41c3b0604a436257c2
AbdulRasheed Oduola <rasheedoduola007@gmail.com>
2025-04-22 17:44:58 +0100

    feat: update form action in index.html and comment out form submission handling in app.js

--- app.js
+++ app.js
@@ -17,15 +17,15 @@ const observer = new IntersectionObserver(entries => {
 
 hiddenTexts.forEach(hiddenText => observer.observe(hiddenText));
 
-document.getElementById("form").addEventListener("submit", async (e) => {
-    e.preventDefault();
-    const formData = new FormData(e.target);
+// document.getElementById("form").addEventListener("submit", async (e) => {
+//     e.preventDefault();
+//     const formData = new FormData(e.target);
   
-    const res = await fetch("http://127.0.0.1:8000/api/submit/", {
-      method: "POST",
-      body: formData
-    });
+//     const res = await fetch("http://127.0.0.1:8000/api/submit/", {
+//       method: "POST",
+//       body: formData
+//     });
   
-    const data = await res.json();
-    alert(data.message);
-  });
+//     const data = await res.json();
+//     alert(data.message);
+//   });
